0a6af1b8b3c493ce795abfa1c638c50a61e8891d
champ <champ6803@gmail.com>
2018-08-15 16:23:19 +0700

    created project

---
+++ wwwroot/js/LoginController.js
@@ -0,0 +1,28 @@
+﻿app.controller("LoginController", function ($scope, $http, AuthenticationService) {
+    $scope.loginForm = typeof loginForm !== "undefined" ? loginForm ? loginForm : null : null;
+
+    $scope.login = function (o) {
+        if (o) {
+            AuthenticationService.Login(o.username, o.password, function (result) {
+                if (result === true) {
+                    alert('ยินดีต้อนรับเข้าสู่ระบบ');
+                    var m = JSON.parse(localStorage.currentUser);
+                    if (m.role == "Recruitment") {
+                        window.location.href = "http://localhost:8002?access_token=" + m.token;
+                    } else if (m.role == "Community") {
+                        window.location.href = "/";
+                        //window.location.href = "http://localhost:8003?access_token=" + m.token;
+                    } else {
+                        window.location.href = "/";
+                    }
+                    
+                } else {
+                    alert('ไม่สามารถเข้าสู่ระบบได้');
+                }
+            });
+        }
+        else {
+            alert("ไม่สามารถเข้าสู่ระบบได้");
+        }
+    };
+});
---
+++ wwwroot/js/MainController.js
@@ -0,0 +1,75 @@
+﻿app.controller("MainController", function ($scope, $http, AuthenticationService) {
+    $scope.currentUser = null;
+    $scope.localStorage = null;
+    $scope.checked = 'notapp';
+    $scope.profile = null;
+
+    $scope.logout = function () {
+        //debugger;
+        delete window.localStorage.currentUser;
+    }
+
+    $scope.customerfocus = function () {
+        //debugger;
+        if (localStorage.length == 1) {
+            if ($scope.currentUser.role == "Recruitment") {
+                alert('ไป customer focus ในบทบาทของ' + $scope.currentUser.role);
+                //window.open('https://localhost:8002/?access_token=' + $scope.currentUser.token);
+                window.location.href = 'http://localhost:8002/?access_token=' + $scope.currentUser.token;
+            }
+            else if ($scope.currentUser.role == "Candidate") {
+                alert('ไป customer focus ในบทบาทของ' + $scope.currentUser.role);
+            }
+            else if ($scope.currentUser.role == "Requirement") {
+                alert('ไป customer focus ในบทบาทของ' + $scope.currentUser.role);
+            }
+        }
+
+    }
+
+    function checkStorage() {
+
+        if (localStorage.length == 1) {
+            $scope.currentUser = JSON.parse(localStorage.currentUser);
+            $scope.currentUser.username = JSON.parse(localStorage.currentUser).username;
+            $scope.currentUser.role = JSON.parse(localStorage.currentUser).role;
+            $scope.currentUser.app = JSON.parse(localStorage.currentUser).app;
+            $scope.currentUser.token = JSON.parse(localStorage.currentUser).token;
+            $scope.checked = 'app';
+        }
+    }
+
+    function getprofile() {
+        //debugger;
+        if (localStorage.length == 1) {
+            var req = {
+                method: 'POST',
+                url: 'http://localhost:3004/api/login/getprofile',
+                params: {
+                    email: JSON.parse(localStorage.currentUser).username
+                },
+                headers: { 'Content-Type': 'application/json' }
+            }
+            $http.defaults.headers.common.Authorization = 'Bearer ' + JSON.parse(localStorage.currentUser).token;
+            $http(req).then(function (response) {
+                //debugger;
+                //alert(response.data);
+                $scope.profile = response.data;
+            }, function (response) {
+                $scope.currentUser = null;
+                $scope.localStorage = null;
+                $scope.checked = 'notapp';
+                $scope.profile = null;
+                AuthenticationService.Logout();
+            });
+
+        } else {
+            console.log('login pls');
+        }
+    }
+
+    $scope.init = function () {
+        checkStorage();
+        getprofile();
+    }
+});
